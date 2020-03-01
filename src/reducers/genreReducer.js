1d3b308dda78c6aa082942074cba899f31dbe35a
cdvx <cedriclusiba@gmail.com>
2020-03-01 15:36:30 +0300

    adding travis and coveralls badge

--- src/reducers/genreReducer.js
+++ src/reducers/genreReducer.js
@@ -1,32 +1,31 @@
 const genresReducer = (state, action) => {
   switch (action.type) {
-
     case 'GENRES': {
       if (action.payload) {
         return {
           ...state,
           genres: action.payload.genres,
-          loading: false
-        }
+          loading: false,
+        };
       }
     }
     case 'GENRES_ERROR': {
       if (action.payload) {
         return {
           ...state,
-          message: action.payload
-        }
+          message: action.payload,
+        };
       }
     }
     default: {
       return state;
     }
   }
-}
+};
 
 const initialState = {
   genres: [],
-  loading: true
-}
+  loading: true,
+};
 
-export default (state = initialState, action) => genresReducer(state, action);
+export default (state = initialState, action) => genresReducer (state, action);
